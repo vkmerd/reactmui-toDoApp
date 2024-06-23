60db8fcbe38a66fa3cc4f3f7af10059e093e955c
vkmerd <mert32139@gmail.com>
2024-06-24 00:22:30 +0300

    formdan gelen to do verileri işlendi ve card componentleri bastırıldı. Sonrasında bu card componentlerindeki to do görevleri için sil ve tamamla işlemleri eklendi, sonrasında bu fonksiyonlar ayrı bir componente taşındı ve App.jsx üzerinden state parametreleri yollandı.

---
+++ src/components/ToDoFunctions.jsx
@@ -0,0 +1,16 @@
+export const handleFormToDo = (e, setToDos) => {
+    e.preventDefault()
+    const toDoData = Object.fromEntries(new FormData(e.target))
+    setToDos(prevToDo => [ ...prevToDo, { task: toDoData.task, completed: false }])
+    e.target.reset()
+}
+
+export const deleteData = (deleteDataIndex, setToDos) => {
+    setToDos(prevToDo => prevToDo.filter((_, index) => index !== deleteDataIndex))
+} 
+
+export const completedData = (completedDataIndex, setToDos) => {
+    setToDos(prevToDo => prevToDo.map((todo, index) => 
+        index === completedDataIndex ? { ...todo, completed: !todo.completed } : todo
+    ))
+}

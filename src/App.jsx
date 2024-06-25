49e3093274c8a58ef494de12163670f8be34f8f5
Mert Öztürk <mert32139@gmail.com>
2024-06-25 20:16:32 +0300

    deploy hatası çözüldü

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,65 @@
 import { useState, useCallback } from 'react';
-import { Button, Container, TextField, Box, Card, CardContent, Grid, Typography, Icon } from '@mui/material';
+import { Button, Container, TextField, Box, Card, CardContent, Grid, Typography } from '@mui/material';
 import { deleteData, handleFormToDo, completedData } from './components/ToDoFunctions';
 
-function App() {
+export default function App() {
   const [todos, setToDos] = useState([]);
 
   const handleForm = useCallback((e) => {
-    handleFormToDo(e, setToDos);
+      handleFormToDo(e, setToDos);
   }, [setToDos]);
 
   const handleDelete = useCallback((index) => {
-    deleteData(index, setToDos);
+      deleteData(index, setToDos);
   }, [setToDos]);
 
   const handleComplete = useCallback((index) => {
-    completedData(index, setToDos);
+      completedData(index, setToDos);
   }, [setToDos]);
+
 
   return (
     <>
-      <Container>
-        <Typography variant="h1" color="inherit" component="div" style={{ textAlign: 'center', fontSize: '50px' }}>
-          To Do App
-        </Typography>
-        <Box component="form" onSubmit={handleForm} display="flex" alignItems="center" sx={{ mt: 1 }}>
-          <TextField 
-            name="task" 
-            placeholder='Görev Listesi Ekleyin' 
-            required 
-            variant="outlined" 
-            fullWidth 
-            margin="normal"
-            style={{ marginRight: '20px' }}
-          />
-          <Button type="submit" variant="contained" color="primary" style={{ height: '56px', width: '150px' }}>
-            Gönder
-          </Button>
-        </Box>
-        <Grid container spacing={3} sx={{ mt: 2 }}>
-          {
-            todos.map((todo, index) => (
-              <Grid item xs={12} sm={6} md={4} key={index}>
-                <Card style={{ backgroundColor: todo.completed ? '#00FF00' : '#FFFFFF', position: 'relative' }}>
-                  <CardContent>
-                    <Typography variant="h6" component="div" style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}> 
-                      {todo.task}
-                    </Typography>
-                    <Button variant="contained" color="secondary" onClick={() => handleDelete(index)} sx={{ mt: 1, mr: 1 }}>
-                      Sil
-                    </Button>
-                    <Button variant="contained" onClick={() => handleComplete(index)} sx={{ mt: 1 }}>
-                      {todo.completed ? 'Geri Al' : 'Tamamla'}
-                    </Button>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))
-          }
-        </Grid>
-      </Container>
+        <Container>
+          <Typography variant="h1" color="inherit" component="div" style={{ textAlign: 'center', fontSize: '50px' }}>
+            To Do App
+          </Typography>
+          <Box component="form" onSubmit={handleForm} display="flex" alignItems="center" sx={{ mt: 1 }}>
+            <TextField 
+              name="task" 
+              placeholder='Görev Listesi Ekleyin' 
+              required 
+              variant="outlined" 
+              fullWidth 
+              margin="normal"
+              style={{ marginRight: '20px' }}
+            />
+            <Button type="submit" variant="contained" color="primary" style={{ height: '56px', width: '150px' }}>
+              Gönder
+            </Button>
+          </Box>
+          <Grid container spacing={3} sx={{ mt: 2 }}>
+            {
+              todos.map((todo, index) => (
+                <Grid item xs={12} sm={6} md={4} key={index}>
+                  <Card style={{ backgroundColor: todo.completed ? '#00FF00' : '#FFFFFF', position: 'relative' }}>
+                    <CardContent>
+                      <Typography variant="h6" component="div" style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}> 
+                        {todo.task}
+                      </Typography>
+                      <Button variant="contained" color="secondary" onClick={() => handleDelete(index)} sx={{ mt: 1, mr: 1 }}>
+                        Sil
+                      </Button>
+                      <Button variant="contained" onClick={() => handleComplete(index)} sx={{ mt: 1 }}>
+                        {todo.completed ? 'Geri Al' : 'Tamamla'}
+                      </Button>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              ))
+            }
+          </Grid>
+        </Container>
     </>
   );
 }
-
-export default App
--- src/components/ToDoFunctions.jsx
+++ src/components/ToDoFunctions.jsx
@@ -1,18 +1,16 @@
-import { useCallback } from 'react';
-
-export const handleFormToDo = useCallback((e, setToDos) => {
+export const handleFormToDo = (e, setToDos) => {
     e.preventDefault();
     const toDoData = Object.fromEntries(new FormData(e.target));
     setToDos(prevToDo => [...prevToDo, { task: toDoData.task, completed: false }]);
     e.target.reset();
-}, []);
+};
 
-export const deleteData = useCallback((deleteDataIndex, setToDos) => {
+export const deleteData = (deleteDataIndex, setToDos) => {
     setToDos(prevToDo => prevToDo.filter((_, index) => index !== deleteDataIndex));
-}, []);
+};
 
-export const completedData = useCallback((completedDataIndex, setToDos) => {
+export const completedData = (completedDataIndex, setToDos) => {
     setToDos(prevToDo => prevToDo.map((todo, index) =>
         index === completedDataIndex ? { ...todo, completed: !todo.completed } : todo
     ));
-}, []);
+};
